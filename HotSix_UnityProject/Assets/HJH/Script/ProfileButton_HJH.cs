499d35c4ae0226fe0376157e54098f76d2978981
JinHo Hong <ghddhksduq@naver.com>
2023-07-11 11:30:47 +0900

    프로필 씬 제작

---
+++ HotSix_UnityProject/Assets/HJH/Script/ProfileButton_HJH.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ProfileButton_HJH : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void MoveProfileScene()
+    {
+        SceneManager.LoadScene("ProfileScene");
+    }
+}
