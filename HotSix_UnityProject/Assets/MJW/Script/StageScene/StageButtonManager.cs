83b389f5902e0d86e3962486181bc27273ff8a4a
JinHo Hong <ghddhksduq@naver.com>
2023-07-18 18:29:05 +0900

    스테이지 진행도

--- HotSix_UnityProject/Assets/MJW/Script/StageScene/StageButtonManager.cs
+++ HotSix_UnityProject/Assets/MJW/Script/StageScene/StageButtonManager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -14,7 +12,8 @@ public class StageButtonManager : MonoBehaviour
 
     private RaycastHit[] hits;
 
-    public void ResetButton(){
+    public void ResetButton()
+    {
         audio.Play();
         selectedIndex = null;
     }
@@ -22,15 +21,20 @@ public class StageButtonManager : MonoBehaviour
     {
         selectedIndex = null;
     }
-    public int? CheckButton(){
+    public int? CheckButton()
+    {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
         hits = Physics.RaycastAll(ray);
-        for(int i = 0; i < hits.Length; ++i){
+        for (int i = 0; i < hits.Length; ++i)
+        {
             RaycastHit hit = hits[i];
-            if(hit.collider.tag == "Button"){
-                for(int j = 0; j < buttons.Length; ++j){
-                    if(System.Object.ReferenceEquals(buttons[j], hit.collider.gameObject)){
+            if (hit.collider.tag == "Button")
+            {
+                for (int j = 0; j < buttons.Length; ++j)
+                {
+                    if (System.Object.ReferenceEquals(buttons[j], hit.collider.gameObject))
+                    {
                         return j + 1;
                     }
                 }
@@ -39,7 +43,8 @@ public class StageButtonManager : MonoBehaviour
         return null;
     }
 
-    public void MoveStage(){
+    public void MoveStage()
+    {
         audio.Play();
         gameManager.stage = (int)selectedIndex;
         Invoke("MoveScene", 0.1f);
@@ -55,11 +60,12 @@ public class StageButtonManager : MonoBehaviour
 
         int count = transform.childCount;
         buttons = new GameObject[count];
-        for(int i = 0; i < count; ++i){
+        for (int i = 0; i < count; ++i)
+        {
             buttons[i] = transform.GetChild(i).gameObject;
         }
         int clearStage = gameManager.userData.staageProgress;
-        for(int i = 0;i < clearStage; ++i)
+        for (int i = 0; i < clearStage; ++i)
         {
             buttons[i].GetComponent<SpriteRenderer>().sprite = buttons[i].GetComponent<StageButton_HJH>().clearButtonIamge;
         }
@@ -69,11 +75,20 @@ public class StageButtonManager : MonoBehaviour
 
     void Update()
     {
-        if(selectedIndex == null && Input.GetMouseButtonDown(0)){
+        if (selectedIndex == null && Input.GetMouseButtonDown(0))
+        {
             selectedIndex = CheckButton();
-            if(selectedIndex != null){
-                audio.Play();
-                StagePopUp.SetActive(true);
+            if (selectedIndex != null)
+            {
+                if (selectedIndex <= GameManager.instance.userData.staageProgress + 1)
+                {
+                    audio.Play();
+                    StagePopUp.SetActive(true);
+                }
+                else
+                {
+                    selectedIndex = null;
+                }
             }
         }
     }
