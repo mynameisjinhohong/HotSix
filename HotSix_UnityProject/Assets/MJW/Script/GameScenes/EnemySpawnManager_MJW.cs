17c88d1981b86252b9c0b23fe1931ade83021369
minjungw00 <minjungw00@naver.com>
2023-07-21 04:55:37 +0900

    유닛 생성 시스템 수정

--- HotSix_UnityProject/Assets/MJW/Script/GameScenes/EnemySpawnManager_MJW.cs
+++ HotSix_UnityProject/Assets/MJW/Script/GameScenes/EnemySpawnManager_MJW.cs
@@ -9,14 +9,72 @@ public class EnemySpawnManager_MJW : MonoBehaviour
 
     public LaneManager_MJW laneManager;
     public TextAsset enemySpawnPatternData;
+    public TextAsset enemySpawnCycleData;
+
     private List<EnemySpawnPattern_MJW> patternData;
-    [Tooltip("패턴 리스트")]
-    public List<int> patternList;
-    private int patternCount;
-    private int indexCount;
-    private float timer;
-    [Tooltip("패턴 반복 시작 인덱스")]
-    public int repeatIndex;
+    private List<List<List<int>>> cycleData;
+    private int laneCount;
+
+    private int[] randomCount;
+    private int[] patternCount;
+    private int[] nodeCount;
+    private float[] selectedSpawnTime;
+    private float[] selectedTotalTime;
+    private int[] selectedUnit;
+    private int[] selectedPattern;
+    private bool[] isSpawned;
+    private float[] timer;
+    
+    [Tooltip("패턴 반복 시작 인덱스, 라인 개수만큼 설정할 것")]
+    public List<int> repeatIndex;
+
+    #endregion
+
+
+    #region Methods
+
+    public void GetPattern(int laneIndex){
+        selectedPattern[laneIndex] = Random.Range(0, cycleData[laneIndex][patternCount[laneIndex]].Count);
+        return;
+    }
+
+    public void GetNode(int laneIndex){
+        EnemySpawnData_MJW node = patternData[cycleData[laneIndex][patternCount[laneIndex]][selectedPattern[laneIndex]]].patternList[nodeCount[laneIndex]];
+        if(node.units[0].id == 0){
+            selectedUnit[laneIndex] = -1;
+            selectedTotalTime[laneIndex] = node.totalTime;
+            selectedSpawnTime[laneIndex] = selectedTotalTime[laneIndex] = 0;
+            return;
+        }
+        selectedUnit[laneIndex] = Random.Range(0, node.units.Count);
+        selectedTotalTime[laneIndex] = node.totalTime;
+        selectedSpawnTime[laneIndex] = Random.Range(node.minTime, node.maxTime);
+        return;
+    }
+
+    public void IncreaseCount(int laneIndex){
+        timer[laneIndex] = 0.0f;
+        ++nodeCount[laneIndex];
+        if(nodeCount[laneIndex] >= patternData[cycleData[laneIndex][patternCount[laneIndex]][selectedPattern[laneIndex]]].Count){
+            ++patternCount[laneIndex];
+            GetPattern(laneIndex);
+            nodeCount[laneIndex] = 0;
+        }
+        if(patternCount[laneIndex] >= cycleData[laneIndex].Count){
+            patternCount[laneIndex] = repeatIndex[laneIndex];
+        }
+        GetNode(laneIndex);
+        isSpawned[laneIndex] = false;
+        return;
+    }
+
+    public void SpawnUnit(int laneIndex){
+        laneManager.SpawnEnemyUnit(laneIndex,
+                                    patternData[cycleData[laneIndex][patternCount[laneIndex]][selectedPattern[laneIndex]]].patternList[nodeCount[laneIndex]].units[selectedUnit[laneIndex]].id,
+                                    patternData[cycleData[laneIndex][patternCount[laneIndex]][selectedPattern[laneIndex]]].patternList[nodeCount[laneIndex]].units[selectedUnit[laneIndex]].level);
+
+        isSpawned[laneIndex] = true;
+    }
 
     #endregion
 
@@ -24,47 +82,99 @@ public class EnemySpawnManager_MJW : MonoBehaviour
     #region Monobehavior Callbacks
 
     void Awake() {
+        // 패턴 저장
         patternData = new List<EnemySpawnPattern_MJW>();
-
         string[] line = enemySpawnPatternData.text.Substring(0, enemySpawnPatternData.text.Length - 1).Split('\n');
-        for(int i = 0; i < line.Length; ++i){
-            string[] row = line[i].Split('\t');
-            int patternIndex = int.Parse(row[0]);
+        int i = 0;
+        int patternIndex = 0;
+        int temp = 0;
+        while(i < line.Length){
+            string[] row1 = line[i].Split('\t');
+            string[] row2 = line[i + 1].Split('\t');
+            EnemySpawnData_MJW data = new EnemySpawnData_MJW();
             int patternCount = patternData.Count;
 
-            if(patternCount <= patternIndex){
+            if(patternCount <= patternIndex){   // 부족한 만큼 patternData 리스트 크기 증가
                 for(int j = 0; j < patternIndex - patternCount + 1; ++j){
                     patternData.Add(new EnemySpawnPattern_MJW());
                 }
             }
 
-            patternData[patternIndex].Add(new EnemySpawnData_MJW(int.Parse(row[1]), int.Parse(row[2]), float.Parse(row[3]), int.Parse(row[4])));
+            if(row1[1] == "" || row1[1] == "\r"){
+                temp = int.Parse(row1[0]);
+                
+                if(temp == 0){                  // 대기 시간 추가
+                    data.AddUnit(0, 0);
+                    patternData[patternIndex].Add(data);
+                    i += 2;
+                    
+                }
+                else{                           // 패턴 번호 확인
+                    patternIndex = temp;
+                    ++i;
+                }
+                continue;
+            }
+
+            // 패턴에 노드 추가
+            for(int j = 0; j < row1.Length; j += 2){
+                if(row1[j] == "" || row1[j] == "\r") break;
+                data.AddUnit(int.Parse(row1[j]), int.Parse(row1[j + 1]));
+            }
+            data.totalTime = float.Parse(row2[0]);
+            data.minTime = float.Parse(row2[1]);
+            data.maxTime = float.Parse(row2[2]);
+            
+            patternData[patternIndex].Add(data);
+            i += 2;
         }
 
-        patternCount = 0;
-        indexCount = 0;
-        timer = 0.0f;
+        // 사이클 저장
+        cycleData = new List<List<List<int>>>();
+        line = enemySpawnCycleData.text.Substring(0, enemySpawnCycleData.text.Length - 1).Split('\n');
+        int firstIndex = -1;
+        int secondIndex = -1;
+        for(i = 0; i < line.Length; ++i){
+            string[] row = line[i].Split('\t');
+            if(row[0] == "0"){
+                ++firstIndex;
+                secondIndex = -1;
+                cycleData.Add(new List<List<int>>());
+            }
+            else{
+                ++secondIndex;
+                cycleData[firstIndex].Add(new List<int>());
+                for(int j = 0; j < row.Length; ++j){
+                    if(row[j] == "" || row[j] == "\r") break;
+                    cycleData[firstIndex][secondIndex].Add(int.Parse(row[j]));
+                }
+            }
+        }
 
+        // 기타 변수 초기화
+        laneCount = cycleData.Count;
+        randomCount = new int[laneCount];
+        patternCount = new int[laneCount];
+        nodeCount = new int[laneCount];
+        selectedSpawnTime = new float[laneCount];
+        selectedTotalTime = new float[laneCount];
+        selectedUnit = new int[laneCount];
+        selectedPattern = new int[laneCount];
+        timer = new float[laneCount];
+        isSpawned = new bool[laneCount];
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        timer += Time.deltaTime;
+        for(int i = 0; i < laneCount; ++i){
+            timer[i] += Time.deltaTime;
 
-        if(timer >= patternData[patternList[patternCount]].patternList[indexCount].spawnTime){
-            laneManager.SpawnEnemyUnit(patternData[patternList[patternCount]].patternList[indexCount].laneIndex,
-                                    patternData[patternList[patternCount]].patternList[indexCount].id,
-                                    patternData[patternList[patternCount]].patternList[indexCount].level);
-
-            timer = 0.0f;
-            ++indexCount;
-            if(indexCount >= patternData[patternList[patternCount]].Count){
-                ++patternCount;
-                indexCount = 0;
+            if(selectedUnit[i] != -1 && !isSpawned[i] && timer[i] >= selectedSpawnTime[i]){
+                SpawnUnit(i);
             }
-            if(patternCount >= patternList.Count){
-                patternCount = repeatIndex;
+            if(timer[i] >= selectedTotalTime[i]){
+                IncreaseCount(i);
             }
         }
     }
--- HotSix_UnityProject/Assets/MJW/Script/GameScenes/EnemySpawnPattern_MJW.cs
+++ HotSix_UnityProject/Assets/MJW/Script/GameScenes/EnemySpawnPattern_MJW.cs
@@ -4,16 +4,37 @@ using UnityEngine;
 
 [System.Serializable]
 public class EnemySpawnData_MJW{
-    public int id;
-    public int level;
-    public float spawnTime;
-    public int laneIndex;
+    
+    public class Unit{
+        public int id;
+        public int level;
 
-    public EnemySpawnData_MJW(int id, int level, float spawnTime, int laneIndex){
-        this.id = id;
-        this.level = level;
-        this.spawnTime = spawnTime;
-        this.laneIndex = laneIndex;
+        public Unit(int id, int level){
+            this.id = id;
+            this.level = level;
+        }
+    }
+
+    public List<Unit> units;
+    public float totalTime;
+    public float minTime;
+    public float maxTime;
+
+    public EnemySpawnData_MJW(){
+        units = new List<Unit>();
+        totalTime = 0;
+        minTime = 0;
+        maxTime = 0;
+    }
+
+    public void AddUnit(int id, int level){
+        units.Add(new Unit(id, level));
+    }
+
+    public void SetTime(float totalTime, float minTime, float maxTime){
+        this.totalTime = totalTime;
+        this.minTime = minTime;
+        this.maxTime = maxTime;
     }
 }
 
