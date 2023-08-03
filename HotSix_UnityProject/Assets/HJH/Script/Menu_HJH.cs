a34993bd5a1430830ea14b99a215848b0b335525
JinHo Hong <ghddhksduq@naver.com>
2023-08-03 12:10:27 +0900

    보상 시스템 클래스 변경

--- HotSix_UnityProject/Assets/HJH/Script/Menu_HJH.cs
+++ HotSix_UnityProject/Assets/HJH/Script/Menu_HJH.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor.Localization.Plugins.XLIFF.V12;
 using UnityEngine;
 using UnityEngine.Localization.Settings;
 using UnityEngine.SceneManagement;
@@ -193,23 +194,16 @@ public class Menu_HJH : MonoBehaviour
     {
         int stage = GameManager.instance.stage;
         RewardData_HJH reward = GameManager.instance.rewardData[stage];
-        if(reward.random)
-        {
-            EverythingRandom(reward.cardAmount);
-        }
-        else
-        {
-
-        }
-    }
-
-    public void EverythingRandom(int count)
-    {
         UserInfo_MJW unitInfo = GameManager.instance.userInfo;
         List<int> unitList = new List<int>();
         List<int> countList = new List<int>();
-        int maxCount = count;
-        while(unitList.Count < 3)
+        //별 받아와서 저 [2]에다가 별 숫자 -1 해서 넣어줄 것
+        int maxCount = reward.startCardAmount[2];
+        if (!reward.random)
+        {
+            unitList.Add(reward.confirmedUnitIdx);
+        }
+        while (unitList.Count < 3)
         {
             int unit = Random.Range(1, unitInfo.userUnitInfo.Count);
             if (unitList.Contains(unit))
@@ -221,20 +215,28 @@ public class Menu_HJH : MonoBehaviour
                 unitList.Add(unit);
             }
         }
-        for(int i =0; i<2; i++)
+        for (int i = 0; i < 2; i++)
         {
-            int ran = Random.Range(1, maxCount - 1);
+            int ran = 0;
+            if(maxCount > 2)
+            {
+                ran = Random.Range(1, maxCount - 1);
+            }
+            else
+            {
+                ran = 1;
+            }
             countList.Add(ran);
             maxCount -= ran;
         }
         countList.Add(maxCount);
-        for(int i = 0; i<3; i++)
+        for (int i = 0; i < 3; i++)
         {
             unitImages[i].sprite = GameManager.instance.unitImage[unitList[i]];
             unitText[i].text = "X " + countList[i];
             GameManager.instance.userInfo.userUnitInfo[unitList[i]].number += countList[i];
         }
-        
+        GameManager.instance.SaveData();
     }
 
     #endregion
