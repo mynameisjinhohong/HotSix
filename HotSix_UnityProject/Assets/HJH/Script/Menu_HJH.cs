361d8f61df7e8160a9c984e365c6c9997766f2c5
JinHo Hong <ghddhksduq@naver.com>
2023-08-04 13:01:31 +0900

    빌드세팅 수정

--- HotSix_UnityProject/Assets/HJH/Script/Menu_HJH.cs
+++ HotSix_UnityProject/Assets/HJH/Script/Menu_HJH.cs
@@ -1,9 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using UnityEditor.Localization.Plugins.XLIFF.V12;
 using UnityEngine;
-using UnityEngine.Localization.Settings;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
