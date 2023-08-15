c897156239f5ce90f48def76ae6ea0de00342634
JinHo Hong <ghddhksduq@naver.com>
2023-08-15 23:02:52 +0900

    test

---
+++ HotSix_UnityProject/Assets/HJH/Script/test_HJH.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_HJH : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //11시 30에 효빈이랑 회의하기로함
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
