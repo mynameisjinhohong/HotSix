a41efd9809a151a6865e79ffa44182ee0ff8ac5f
JinHo Hong <ghddhksduq@naver.com>
2023-08-15 22:51:58 +0900

    테스트

---
+++ HotSix_UnityProject/Assets/HJH/Script/test_HJH.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_HJH : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //지금 나의 주석이 읽힌다면 읽힌다고 말하셈.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
