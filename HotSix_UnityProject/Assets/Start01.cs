53219d1f29b89ce2e8c0cd0a37d4b2c8ba3f1717
uriham <113496516+uriham@users.noreply.github.com>
2023-08-29 17:10:59 +0900

    add the fixed scene
    plz check :)

--- HotSix_UnityProject/Assets/Start01.cs
+++ HotSix_UnityProject/Assets/Start01.cs
@@ -12,9 +12,9 @@ public class Start01 : MonoBehaviour
 
     private void OnEnable(){
         star1.localPosition = new Vector2(Screen.width, 202f);
-        star1.LeanMoveLocalX(0,1f).setEaseOutBack();
+        star1.LeanMoveLocalX(0,1f).setEaseOutBack().delay = 0.5f;
 
         star2.localPosition = new Vector2(Screen.width,-380f);
-        star2.LeanMoveLocalX(0,1f).setEaseOutBack().delay = 1f;
+        star2.LeanMoveLocalX(0,1f).setEaseOutBack().delay = 1.5f;
     }
 }
